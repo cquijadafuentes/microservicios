b693876a41babe93d2ff8dfad4c7f1076a4d7ff1
Carlos Quijada-Fuentes <cquijadafuentes@gmail.com>
2021-06-18 16:19:02 -0400

    Definición de API con los servicios para pet-store.

--- proyecto/app/home/views.py
+++ proyecto/app/home/views.py
@@ -21,14 +21,40 @@ def dashboard():
     Render the dashboard template on the /dashboard route
     """
     return render_template('home/dashboard.html', title="Dashboard")
-    
 
-@home.route('/listall')
+
+@home.route('/api/listall')
 def listatodos():
     return "Listando todos los items:"
-    
 
-@home.route('/list/<iduser>')
-def listaporuser(iduser):
-    x = "listando items de " + iduser
+
+@home.route('/api/list/<iduser>')
+def listaporuser(iduser=0):
+    x = "listando items de " + str(iduser)
     return x
+
+
+@home.route('/api/update/<iduser>/<idprod>/<newcant>')
+def modificacant(iduser=0, idprod=0, newcant=0):
+    x = "Actualizando en cliente " + str(iduser) + " el producto " + str(idprod) + " con cantidad " + str(newcant)
+    return x
+
+
+@home.route('/api/delete/<iduser>/<idprod>')
+def eliminaitem(iduser=0, idprod=0):
+    x = "Eliminando el item " + str(idprod) + " del carrito del cliente " + str(iduser)
+    return x
+
+
+@home.route('/api/create/<iduser>/<idprod>/<valor>')
+def insertaunitem(iduser=0, idprod=0, valor=0):
+    x = "Creando item en carrito del cliente " + str(iduser) + " el producto " + str(idprod) + " con valor unitario: $" + str(valor)
+    return x
+
+
+@home.route('/api/create/<iduser>/<idprod>/<cantidad>/<valor>')
+def insertaitems(iduser=0, idprod=0, cantidad=0, valor=0):
+    x = "Creando item en carrito del cliente " + str(iduser) + " el producto " + str(idprod) + ", cantidad: " + str(cantidad) + " con valor unitario: $" + str(valor)
+    return x
+
+
